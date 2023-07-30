525f16f7326ad3bd9947a2c310ca6a422aa49ff6
velcronator <valacos7@gmail.com>
2023-07-30 23:13:24 +1000

    using unity events

--- Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.UI.DamageText
@@ -6,15 +7,10 @@ namespace RPG.UI.DamageText
     public class DamageTextSpawner : MonoBehaviour
     {
         [SerializeField] DamageText damageTextPrefab = null;
-        void Start()
-        {
-            Spawn(55f);
-        }
 
         public void Spawn(float damageAmount)
         {
             DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
         }
-
     }
 }
