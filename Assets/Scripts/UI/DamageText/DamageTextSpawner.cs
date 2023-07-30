a14d57e9b460fe2b3589cc5e3566db8b98b577c3
velcronator <valacos7@gmail.com>
2023-07-30 22:02:42 +1000

    Spawning Damage Text

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+        void Start()
+        {
+            Spawn(55f);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+
+    }
+}
