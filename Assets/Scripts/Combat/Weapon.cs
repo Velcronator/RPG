e47494b6e5d7303c6272c96bbe241e290178e045
velcronator <valacos7@gmail.com>
2024-03-19 15:06:53 +1100

    Boom Microphone Audio Listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-            print("Weapon hit" + gameObject.name);
+            Debug.Log("Weapon Hit" + gameObject.name);
+            onHit.Invoke();
         }
     }
 }
